1f85ed9deec82533063758b507da10631c1fe407
Serafin8 <ser.chuzhmarov@ya.ru>
2021-01-31 09:47:30 +0300

    Initial commit

--- lesson1task3.py
+++ lesson1task3.py
@@ -1,1 +1,8 @@
 # Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
+num = int(input("Введите число для произведения рассчета: "))
+temp = str(num)
+num1 = temp + temp
+num2 = temp + temp + temp
+amount = num + int(num1) + int(num2)
+print("Результат: ", num, "+", num, num, "+", num, num, num, "=", amount)
+
